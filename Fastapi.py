194b332f6078a80dfe2460d916901a4ae0414c36
EbadaHamdy <Ebadahamdy138@gmail.com>
2024-06-22 23:20:50 +0300

    Update Fastapi.py

--- Fastapi.py
+++ Fastapi.py
@@ -202,4 +202,4 @@ def add_funds(plan_num: int, additional_funds: float):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
